f946109fa2e7f431081cd88d885c06e520fc897b
Kenzi Chen <a23g32@gmail.com>
2020-09-21 16:04:01 +0800

    Updates

---
+++ precache-manifest.cff6b3e85f10998eecfd9420c4b49fad.js
@@ -0,0 +1,58 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "88c2d9c81b9e49d5733cfe0ed8e00196",
+    "url": "/index.html"
+  },
+  {
+    "revision": "c9aca3556ee70596128b",
+    "url": "/static/css/2.3c03f26f.chunk.css"
+  },
+  {
+    "revision": "14056e7668b05f6a30bf",
+    "url": "/static/css/main.9ab3e605.chunk.css"
+  },
+  {
+    "revision": "c9aca3556ee70596128b",
+    "url": "/static/js/2.4d7a4b97.chunk.js"
+  },
+  {
+    "revision": "6287c65a6963df837d60dc49bb220910",
+    "url": "/static/js/2.4d7a4b97.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "14056e7668b05f6a30bf",
+    "url": "/static/js/main.d8a8eb33.chunk.js"
+  },
+  {
+    "revision": "847be10bac21d4dfc1fe",
+    "url": "/static/js/runtime-main.bdf90e09.js"
+  },
+  {
+    "revision": "c741f58ea897ab28cbc25e4b6767dc30",
+    "url": "/static/media/credit-card-regular.c741f58e.svg"
+  },
+  {
+    "revision": "fa2c07daf87d1ec998638f37e1412080",
+    "url": "/static/media/home-solid.fa2c07da.svg"
+  },
+  {
+    "revision": "8503b4e47477b35519e7c8a285de8650",
+    "url": "/static/media/minus-solid.8503b4e4.svg"
+  },
+  {
+    "revision": "b99e8ee27ac446e7f85fd8bb73d72aae",
+    "url": "/static/media/plus-solid.b99e8ee2.svg"
+  },
+  {
+    "revision": "8bf1af37dd02198b256725e83a203795",
+    "url": "/static/media/shopping-cart-solid.8bf1af37.svg"
+  },
+  {
+    "revision": "fd89e316a694f6a82f438e7b142d9a1a",
+    "url": "/static/media/store-solid.fd89e316.svg"
+  },
+  {
+    "revision": "bf0aabb525033ba49220321cde5b3339",
+    "url": "/static/media/taylor-kiser-EvoIiaIVRzU-unsplash.bf0aabb5.jpg"
+  }
+]);
