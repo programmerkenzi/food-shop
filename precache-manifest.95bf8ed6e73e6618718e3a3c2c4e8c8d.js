6bf5842f5530f9f668994150533f34134c832764
Kenzi Chen <a23g32@gmail.com>
2020-09-21 18:28:28 +0800

    Updates

---
+++ precache-manifest.95bf8ed6e73e6618718e3a3c2c4e8c8d.js
@@ -0,0 +1,58 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c08d7a7ee325e5b568b39f2587c55b5a",
+    "url": "/food-shop/index.html"
+  },
+  {
+    "revision": "5c3ec8bbe6d36243100e",
+    "url": "/food-shop/static/css/2.3c03f26f.chunk.css"
+  },
+  {
+    "revision": "94fb11001671a84468cb",
+    "url": "/food-shop/static/css/main.51e57780.chunk.css"
+  },
+  {
+    "revision": "5c3ec8bbe6d36243100e",
+    "url": "/food-shop/static/js/2.31c61958.chunk.js"
+  },
+  {
+    "revision": "6287c65a6963df837d60dc49bb220910",
+    "url": "/food-shop/static/js/2.31c61958.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "94fb11001671a84468cb",
+    "url": "/food-shop/static/js/main.1c8a3e14.chunk.js"
+  },
+  {
+    "revision": "ea4abb6620907bd609f6",
+    "url": "/food-shop/static/js/runtime-main.75ae7fcb.js"
+  },
+  {
+    "revision": "c741f58ea897ab28cbc25e4b6767dc30",
+    "url": "/food-shop/static/media/credit-card-regular.c741f58e.svg"
+  },
+  {
+    "revision": "fa2c07daf87d1ec998638f37e1412080",
+    "url": "/food-shop/static/media/home-solid.fa2c07da.svg"
+  },
+  {
+    "revision": "8503b4e47477b35519e7c8a285de8650",
+    "url": "/food-shop/static/media/minus-solid.8503b4e4.svg"
+  },
+  {
+    "revision": "b99e8ee27ac446e7f85fd8bb73d72aae",
+    "url": "/food-shop/static/media/plus-solid.b99e8ee2.svg"
+  },
+  {
+    "revision": "8bf1af37dd02198b256725e83a203795",
+    "url": "/food-shop/static/media/shopping-cart-solid.8bf1af37.svg"
+  },
+  {
+    "revision": "fd89e316a694f6a82f438e7b142d9a1a",
+    "url": "/food-shop/static/media/store-solid.fd89e316.svg"
+  },
+  {
+    "revision": "bf0aabb525033ba49220321cde5b3339",
+    "url": "/food-shop/static/media/taylor-kiser-EvoIiaIVRzU-unsplash.bf0aabb5.jpg"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.cff6b3e85f10998eecfd9420c4b49fad.js"
+  "/food-shop/precache-manifest.95bf8ed6e73e6618718e3a3c2c4e8c8d.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/food-shop/index.html"), {
   
   blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
 });
